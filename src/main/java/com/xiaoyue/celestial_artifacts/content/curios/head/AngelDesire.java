32976c3721808195a2d64132bda719d08458128b
shenenxiaoyue <3371025687@qq.com>
2026-08-31 17:45:21 +0800

    1.6.5

--- src/main/java/com/xiaoyue/celestial_artifacts/content/curios/head/AngelDesire.java
+++ src/main/java/com/xiaoyue/celestial_artifacts/content/curios/head/AngelDesire.java
@@ -17,9 +17,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ElytraItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.Nullable;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 
 import java.util.List;
+import java.util.Optional;
 
 @SerialClass
 public class AngelDesire implements TickFacet, MultiLineText, CAAttackToken {
@@ -34,9 +38,14 @@ public class AngelDesire implements TickFacet, MultiLineText, CAAttackToken {
 		return stack.getItem() instanceof ElytraItem || stack.is(CATagGen.ANGEL_DESIRE_COMPAT);
 	}
 
+	public boolean hasElytraCurio(LivingEntity entity) {
+		Optional<ICuriosItemHandler> inv = CuriosApi.getCuriosInventory(entity).resolve();
+        return inv.map(handler -> handler.findFirstCurio(this::isCurrent).isPresent()).orElse(false);
+    }
+
 	@Override
 	public void tick(LivingEntity entity, ItemStack stack) {
-		if (entity.isFallFlying() && isCurrent(entity.getItemBySlot(EquipmentSlot.CHEST))) {
+		if (entity.isFallFlying() && (isCurrent(entity.getItemBySlot(EquipmentSlot.CHEST)) || hasElytraCurio(entity))) {
 			CCUtils.addFlySpeed(entity, 1f);
 		}
 	}
